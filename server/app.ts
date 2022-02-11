8b1c73efa45f48d5c986b65e326a56842b37b913
Ryan Lott <ryanclott@outlook.com>
2022-02-10 22:54:36 -0600

    Initial App Setup

---
+++ server/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+
+app.use(express.json());
+// Serve the React static files after build
+app.use(express.static("../client/build"));
+
+app.get("/api/hello", (req, res) => {
+  res.send({ message: "Hello" });
+});
+
+// All other unmatched requests will return the React app
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+export default app;
