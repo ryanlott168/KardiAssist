2d5f2f49991d2ec094d7a9a873473a19efa6b54a
Ryan Lott <ryanclott@outlook.com>
2022-02-27 12:30:43 -0600

    updated auth middleware

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,10 +1,9 @@
 
-const isLoggedIn = (req, res, next)  => {
-  req.user ? next() : res.sendStatus(401);
+export function isLoggedIn (req, res, next) {
+  req.isAuthenticated() ? next() : res.sendStatus(401);
 }
 
-export default {
-  isLoggedIn
-};
-
+export function isAdmin (req, res, next) {
+  req.isAuthenticated() && req.user['isAdmin'] ? next() : res.sendStatus(401);
+}
 
