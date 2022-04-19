22ba93e07b9f4540efc6a942c90ca164d10a5c19
Ryan Lott <ryanclott@outlook.com>
2022-04-18 20:10:50 -0500

    study backend controllers

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 import user from './user';
 import followUpTask from './followUpTask';
+import study from './study';
 
 mongoose.Promise = global.Promise;
 
 const db = {
     mongoose,
     user,
-    followUpTask
+    followUpTask,
+    study
 };
 
 export default db;
