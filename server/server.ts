daea1a349c8a1f0604603f1584589f0e5fcee039
Ryan Lott <ryanclott@outlook.com>
2022-04-18 20:06:05 -0500

    Added login for DB

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import dbConfig from './config/db';
 const port = process.env.PORT || 5001;
 
 // Connect to DB
-db.mongoose.connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`).then(() => {
+db.mongoose.connect(`mongodb://${dbConfig.USER}:${dbConfig.PWD}@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`).then(() => {
     console.log('Successfully connect to MongoDB.');
 })
 .catch(err => {
